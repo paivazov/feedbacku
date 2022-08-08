2075c566b56b8b15569af5c09c8a58fb2b87335e
Pavlo Aivazov <72413229+paivazov@users.noreply.github.com>
2022-08-08 17:11:38 +0300

    feat: implement swagger to project (#4)

--- FeedbackU/urls.py
+++ FeedbackU/urls.py
@@ -17,8 +17,31 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from rest_framework.permissions import AllowAny
+
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="FeedbackU API",
+        default_version='v0.1',
+    ),
+    public=True,
+    permission_classes=[AllowAny],
+)
 
 urlpatterns = [
+    path(
+        'swagger/',
+        schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui',
+    ),
+    path(
+        'redoc/',
+        schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc',
+    ),
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
     path("organisation/", include('organisations.urls')),
